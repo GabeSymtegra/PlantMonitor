99b53af64f2080c239fff2f8b9754e9cfbcb1519
GabeSymtegra <gabe@symtegracontrols.com>
2026-07-01 12:09:22 -0400

    folder creation

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8f76ce21675341d8d4740d163a58ed1370ba03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f5990f8367d748105ff12569d322b984064b47")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
