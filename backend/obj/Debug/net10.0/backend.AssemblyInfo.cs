8b031a17186afeb3fa12c0a3dd81f93a9f65f20a
GabeSymtegra <gabe@symtegracontrols.com>
2026-07-09 14:18:02 -0400

    test

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28fc3f30910a624ed0171c72713ffa18ca27b3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a13a9d370da14930da69aab399fbca4720d2a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
